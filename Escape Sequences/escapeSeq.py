8bcdf0f7eb478cda1bd0fb9378d4f55aa8e2c9f9
Arun Kumar <kumar05.962013@gmail.com>
2020-09-05 11:40:49 +0530

    initial commit

---
+++ Escape Sequences/escapeSeq.py
@@ -0,0 +1,34 @@
+# Escape Character
+# \
+
+# Escape Sequences
+
+# \"
+# \'
+# \\
+# \n newLine
+
+
+
+
+
+course = "python \"Programming"
+
+print(course)
+# o/p : python "Programming
+
+course = "python \'Programming"
+
+print(course)
+# o/p : python 'Programming
+
+course = "python \\Programming"
+
+print(course)
+# o/p : python \Programming
+
+course = "python \nProgramming"
+
+print(course)
+# o/p : python
+#       Programming
---
+++ app.py
@@ -0,0 +1,20 @@
+# print("Hello World")
+# print("*" * 10);
+#
+# price= 10
+# boolean = 0
+# print(boolean)
+# name = input("Enter Name: ")
+# color = input("What is your favorite Color: ")
+# print(name+ ' Likes ' + color)
+
+# birth_year = input('Birth year :  ')
+# age = 2020 - int(birth_year)
+# print(age)
+
+# weight_lbs = input("Weight lbs: ")
+# w_kg =int(weight_lbs) * 0.45
+# print(w_kg)
+
+name = "Arun"
+print(name[1:-1])
