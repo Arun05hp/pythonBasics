b7823187a75af66b53d01b22f42908dcaf6a8004
Arun Kumar <kumar05.962013@gmail.com>
2020-09-05 22:00:36 +0530

    Type Conversion

---
+++ Numbers/number.py
@@ -0,0 +1,20 @@
+import math
+
+x = 1
+x = 1.1
+x =  1 + 2j # a + bi
+
+print("add",10 + 3)
+print("sub",10 - 3)
+print("mul",10 * 3)
+print("div",10 / 3)  # 3.3333333333
+print("//",10 // 3)  # 3    return in integer after division
+print("mod",10 % 3)  # 1
+print("^",10 ** 3)  # 10^3 =1000
+
+print(round(23.33)) # 23
+
+print(abs(-2.9))  # 2.9
+
+print(math.ceil(2.2))  # 3
+print(math.floor(2.2))  # 2
---
+++ Type Conversion/typeConv.py
@@ -0,0 +1,27 @@
+# x = input("X: ")
+#
+# y = int(x) + 1
+#
+# print(y)
+# print(f"x: {x}, y: {y} ")
+# X: 12
+# 13
+# x: 12, y: 13
+
+###  Falsy Value
+
+# ""
+# 0
+# None
+
+print(bool(""))
+# False
+
+print(bool(-1))
+#  True
+
+print(bool("False"))
+# True
+
+print(bool(None))
+# False
