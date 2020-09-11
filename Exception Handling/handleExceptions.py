84e3a0fa5902c549dd43d412ac90f78979ec013b
Arun Kumar <kumar05.962013@gmail.com>
2020-09-11 21:43:23 +0530

    handling multiple exceptions

--- Exception Handling/handleExceptions.py
+++ Exception Handling/handleExceptions.py
@@ -3,3 +3,19 @@ try:
 except ValueError as ex:
     print("You didn't enter a valid age")
     print(ex)
+
+#### handling multiple exceptions ####
+try:
+    age = int(input("Age: "))
+    x = 10/age
+except (ValueError,ZeroDivisionError):
+    print("You didn't enter a valid age")
+   
+
+try: 
+    file = open("../app.py")
+    num=int(input("Enter number: "))
+except ValueError:
+    print("Not valid number")
+finally:     #this final clause is always executed.
+    file.close()
