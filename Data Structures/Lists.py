1b2a9927f2f7df0cebc16ba628a704388181f0fd
Arun Kumar <kumar05.962013@gmail.com>
2020-09-11 21:33:15 +0530

    exceptions

--- Data Structures/Lists.py
+++ Data Structures/Lists.py
@@ -33,3 +33,9 @@ print(numbers[::-1])   # step 2
 str1,str2 =strings
 print(str1,str2)
 #> ak sk
+
+first,sec, *other = numbers  # numbers = [0,1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(sec)
+#> 0
+print(other)
+#> [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
---
+++ Exception Handling/handleExceptions.py
@@ -0,0 +1,5 @@
+try:
+    age = int(input("Age: "))
+except ValueError as ex:
+    print("You didn't enter a valid age")
+    print(ex)
