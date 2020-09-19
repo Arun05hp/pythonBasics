2f07b4442b4784d60fc5039c7a351339eee2a776
Arun Kumar <kumar05.962013@gmail.com>
2020-09-19 17:02:22 +0530

    comparison magic method

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -44,17 +44,40 @@
 
 ######## Magic Methods ########
 
-class Point:
-    def __init__(self,x,y):    # __init__    magic method.on print this return __main__
-        self.x = x
-        self.y = y
-    def __str__(self):           # __str__    magic method
-        return f"({self.x},{self.y})"
+# class Point:
+#     def __init__(self,x,y):    # __init__    magic method.on print this return __main__
+#         self.x = x
+#         self.y = y
+#     def __str__(self):           # __str__    magic method
+#         return f"({self.x},{self.y})"
 
-    def draw(self):
-        print(f" Point ({self.x} , {self.y})")
+#     def draw(self):
+#         print(f" Point ({self.x} , {self.y})")
+
+# point = Point(1,2)
+# point.draw()
+# print(point)
+# print(str(point))
+
+
+
+######  comparison magic method   ########
+
+class Point:
+    def __init__(self,x,y): 
+        self.x = x      
+        self.y = y
+
+    def __eq__(self,other):         
+        return self.x == other.x and self.y == other.y
+    
+    def __gt__(self,other):
+        return self.x > other.x and self.y > other.y
+    
+
 
 point = Point(1,2)
-point.draw()
-print(point)
-print(str(point))
+other = Point(10,20)
+print(point == other)
+print(point < other)
+print(point > other)
