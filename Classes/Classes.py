491bb662ffe44eae2fba4f5aba908cf714228a34
Arun Kumar <kumar05.962013@gmail.com>
2020-09-19 17:09:27 +0530

    arithmetic magin methods

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -63,21 +63,41 @@
 
 ######  comparison magic method   ########
 
+# class Point:
+#     def __init__(self,x,y): 
+#         self.x = x      
+#         self.y = y
+
+#     def __eq__(self,other):         
+#         return self.x == other.x and self.y == other.y
+    
+#     def __gt__(self,other):
+#         return self.x > other.x and self.y > other.y
+    
+
+
+# point = Point(1,2)
+# other = Point(10,20)
+# print(point == other)
+# print(point < other)
+# print(point > other)
+
+
+############   Numeric magic method  ############
+
 class Point:
-    def __init__(self,x,y): 
-        self.x = x      
+    def __init__(self,x,y):
+        self.x = x
         self.y = y
 
-    def __eq__(self,other):         
-        return self.x == other.x and self.y == other.y
-    
-    def __gt__(self,other):
-        return self.x > other.x and self.y > other.y
-    
+    def __str__(self):         
+        return f"({self.x},{self.y})"
 
+    def __add__(self,other):
+        return Point(self.x + other.x , self.y + other.y)
 
-point = Point(1,2)
-other = Point(10,20)
-print(point == other)
-print(point < other)
-print(point > other)
+point = Point(10,20)
+other = Point(1,20)
+result = point + other
+print(result.x)
+print(point + other)
